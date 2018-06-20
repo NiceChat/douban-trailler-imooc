b8f6db7a147dc89d34951e9891b23d6e99774e99
yangleilei <oddboy1990@gmail.com>
2018-06-20 15:28:40 +0800

    集成中间件的模板引擎

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const Koa = require('koa')
+const { resolve } = require('path')
 const app = new Koa()
+// const { htmlTpl } = require('./tpl')
+// const { ejsTpl } = require('./tpl')
+// const { pugTpl } = require('./tpl')
+// const ejs = require('ejs')
+// const pug = require('pug')
+const views = require('koa-views')
+
+app.use(views(resolve(__dirname, './views'), { 
+    extension: 'pug' 
+  }))
 
 app.use(async (ctx, next) => {
-  ctx.body = '电影首页'
-  await next()
+  // ctx.type = "text/html; charset=utf-8"
+  // ctx.body = ejs.render(ejsTpl, {
+  //   name: 'Yangleilei'
+  // })  
+  // ctx.body = pug.render(pugTpl)
+  // await next()
+  await ctx.render('index', {
+    title: 'Pug-Middleware',
+    name: 'Yangleilei',
+  })
 })
 
 app.listen(4455)
