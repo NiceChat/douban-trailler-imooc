7b575323f11495512d6b3a0805b7fdc590fff701
yangleilei <oddboy1990@gmail.com>
2018-06-22 17:38:54 +0800

    添加爬取视屏

---
+++ server/crawler/crawlerVideo.js
@@ -0,0 +1,61 @@
+const puppeteer = require('puppeteer')
+const videoUrl = 'https://movie.douban.com/subject/'
+const id = 27615874
+
+const sleep = time => new Promise(reslove => {
+  setTimeout(reslove, time)
+})
+
+;(async () => {
+  console.log('Start visit the target page.')
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.goto(`${videoUrl}${id}`, {waitUntil: 'networkidle2'})
+  await sleep(1000)
+
+  const cover = await page.evaluate(() => {
+    var $ = window.$
+    var $cover = $('.related-pic-video')
+
+    if ($cover) {
+      var link = $cover.attr('href')
+      var cover = $cover.css('backgroundImage')
+      var reg = /https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*/gi
+
+      return {
+        link,
+        cover: cover.match(reg)[0]
+      } 
+    }
+
+    return {}
+  })
+
+  let video 
+  if (cover.link) {
+    await page.goto(cover.link, {waitUntil: 'networkidle2'})
+    await sleep(2000)
+
+    video =  await page.evaluate(() => {
+      var $ = window.$
+      var $source = $('source')
+
+      if ($source) {
+        return $source.attr('src')
+      }
+
+      return ''
+    })
+  }
+
+  var data = {
+    id: id,
+    cover: cover.cover,
+    video: video 
+  }
+  await browser.close()
+
+  process.send({data})
+  process.exit(0)
+})()
--- server/tasks/video.js
+++ server/tasks/video.js
@@ -2,7 +2,7 @@ const cp = require('child_process')
 const { resolve } = require('path')
 
 ;(async () => {
-  const script = resolve(__dirname, '../crawler/puppeteer')
+  const script = resolve(__dirname, '../crawler/crawlerVideo')
   const child = cp.fork(script, []) 
   let invoked = false
 
@@ -21,7 +21,7 @@ const { resolve } = require('path')
   })
 
   child.on('message', data => {
-    let result = data.result  
+    let result = data
 
     console.log(result)
   })
